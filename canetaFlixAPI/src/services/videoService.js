a44690549592418903c0a89c3f6e818ff151c392
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-12 17:31:02 -0300

    exclusão de comentários

--- canetaFlixAPI/src/services/videoService.js
+++ canetaFlixAPI/src/services/videoService.js
@@ -35,35 +35,4 @@ async function inserirCategoriasFilmes(db, filme){
     }
 }
 
-// // Função principal, requisita a lista de filmes mais populares da API e trata a resposta chamando as outras funções
-// async function getFilmesPopulares(db, API_KEY){
-//     console.log('\nInserindo Filmes Populares...........');
-//     console.log('\n{');
-
-//     for (let page = 1; page < 4; page++) {
-//         const url = `https://api.themoviedb.org/3/movie/popular?language=pt-br&page=${page}&region=brazil`;
-//         const options = {
-//             method: 'GET',
-//             headers: {
-//                 accept: 'application/json',
-//                 Authorization: 'Bearer ' + API_KEY
-//             }
-//         };
-
-//         const res = await fetch(url, options);
-//         const json = await res.json();
-
-//         const filmesPopulares = JSON.parse(JSON.stringify(json));
-
-//         for (const filme of filmesPopulares.results) {
-//             console.log(`Filme ${filme.title} inserido com sucesso! {`);
-//             await inserirMetadadosFilmes(db, filme, tipo_video);
-//             await inserirFilmes(db, filme, tipo_video);
-//             await inserirCategoriasFilmes(db, filme);
-//             console.log('}\n');
-//         }
-//     }
-//     console.log('}\n');
-// }
-
 module.exports = { inserirFilmes, inserirCategoriasFilmes};
