1e7a0f1cb1fb211c2bac557fce1cb277cc67a441
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-12 17:47:24 -0300

    Liberando acesso a api na rede

--- canetaFlixAPI/server.js
+++ canetaFlixAPI/server.js
@@ -1,6 +1,8 @@
 const app = require('./src/app.js');
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+const HOST = process.env.HOST || '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`);
 });
