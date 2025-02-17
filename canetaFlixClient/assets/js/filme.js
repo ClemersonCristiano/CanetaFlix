0b30de95aaf6157de01aab12ccb97af9a29c6e65
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-17 20:12:44 -0300

    criando página de filme dinamicamente

--- canetaFlixClient/assets/js/filme.js
+++ canetaFlixClient/assets/js/filme.js
@@ -4,17 +4,20 @@ console.log(`ID do filme: ${idFilme}`);
 const dadosFilme = document.getElementById('dadosFilme');
 
 
-// fetch(`http://localhost:3000/api/videos/filmes/${idFilme}`)
-// .then(response => response.json()).then(json => {
+fetch(`http://localhost:3000/api/videos/filmes/${idFilme}`)
+.then(response => response.json())
+.then(json => {
 
-//     console.log(json);
-// })
+    const filme = json.dadosFilme;
 
+    dadosFilme.innerHTML = `
+        
+        <iframe src="${filme.url}"> </iframe>
 
+        <section class="containerInfosFilme"> 
+            <h1>${filme.titulo}</h1>
+            <p>${filme.descricao}</p>
+        </section>
+    `;
 
-
-dadosFilme.innerHTML = `
-
-    <iframe src="" id="SuperFlixAPIContainerVideo"></iframe>
-
-`;
+})
