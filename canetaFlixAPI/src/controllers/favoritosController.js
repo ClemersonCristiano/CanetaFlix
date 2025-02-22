c05cf91d2f7a3191fe72481fe6cd382f77ffc663
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-22 16:45:47 -0300

    Controller de favoritos

---
+++ canetaFlixAPI/src/controllers/favoritosController.js
@@ -0,0 +1,66 @@
+const favoritosModel = require('../models/favoritosModel');
+
+exports.listarFavoritos = async (req, res) => {
+    try {
+        
+        const id_usuario = req.params.id_usuario;
+        const favoritos = await favoritosModel.getAllFavoritos(id_usuario);
+
+        res.json({ 
+            success: true, 
+            message: "Favoritos recuperados com sucesso", 
+            total: favoritos.length, dadosFavoritos: favoritos 
+        }); 
+
+    } catch (error) {
+        console.error("Erro ao buscar favoritos:", error);
+        res.status(500).json({ 
+            success: false, 
+            message: "Erro ao buscar favoritos", error: error.message 
+        }); 
+    }
+};
+
+exports.adicionarFavorito = async (req, res) => {
+    try {
+        const id_usuario = req.params.id_usuario;
+        const id_video_api = req.params.id_video_api;
+
+        const favorito = await favoritosModel.adicionarFavorito(id_usuario, id_video_api);
+
+        res.json({ 
+            success: true, 
+            message: "Favorito adicionado com sucesso", 
+            dadosFavorito: favorito 
+        }); 
+
+    } catch (error) {
+        console.error("Erro ao adicionar favorito:", error);
+        res.status(500).json({ 
+            success: false, 
+            message: "Erro ao adicionar favorito", error: error.message 
+        }); 
+    }    
+};
+
+exports.removerFavorito = async (req, res) => {
+    try {
+        const id_usuario = req.params.id_usuario;
+        const id_video_api = req.params.id_video_api;
+
+        const favorito = await favoritosModel.removerFavorito(id_usuario, id_video_api);
+
+        res.json({ 
+            success: true, 
+            message: "Favorito removido com sucesso", 
+            dadosFavorito: favorito 
+        }); 
+
+    } catch (error) {
+        console.error("Erro ao remover favorito:", error);
+        res.status(500).json({ 
+            success: false, 
+            message: "Erro ao remover favorito", error: error.message 
+        }); 
+    }    
+};
