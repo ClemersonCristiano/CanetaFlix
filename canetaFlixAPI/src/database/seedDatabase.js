f395a90a5d868602e7ebd994b36d74e218ac9c03
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-23 18:20:27 -0300

    função que insere usuário admin

--- canetaFlixAPI/src/database/seedDatabase.js
+++ canetaFlixAPI/src/database/seedDatabase.js
@@ -7,6 +7,40 @@ const db = require('../config/DBconnection.js');
 const API_KEY = process.env.API_KEY;
 const tipo_video = 'filme';
 
+async function inserirTipoUser() {
+    
+    const queryTipoUser = 'INSERT INTO tipo_usuario (nome) VALUES ("Administrador"), ("Usuário")';
+    await new Promise((resolve, reject) => {
+        db.query(queryTipoUser, (err, results) => {
+            if (err) {
+                console.error('Erro ao inserir tipos de usuário:', err);
+                reject(err);
+            } else {
+                console.log('Tipos de usuário inseridos com sucesso:', results);
+                resolve(results);
+            }
+        });
+    });
+
+}
+async function inserirUsuarioBD() {
+
+    const pw = bcrypt.hashSync('admin', 10);
+    const data_cadastro = new Date().toISOString().split('T')[0];
+    const queryUser = 'INSERT INTO usuarios (nome, email, pw, data_cadastro, status, id_tipo_usuario) VALUES ("admin", "admin@admin", ?, ? , "ativo", 1)';
+    await new Promise((resolve, reject) => {
+        db.query(queryUser, [pw], [data_cadastro], (err, results) => {
+            if (err) {
+                console.error('Erro ao inserir usuário:', err);
+                reject(err);
+            } else {
+                console.log('Usuário inserido com sucesso:', results);
+                resolve(results);
+            }
+        });
+    })
+}
+
 // Função principal, requisita a lista de filmes mais populares da API e trata a resposta chamando as outras funções
 async function getFilmesPopulares(db, API_KEY){
     console.log('\nInserindo Filmes Populares...........');
@@ -39,8 +73,10 @@ async function getFilmesPopulares(db, API_KEY){
     console.log('}\n');
 }
 
-// Função principal para garantir a ordem de execução
+// Função para garantir a ordem de execução
 async function main() {
+    await inserirTipoUser();
+    await inserirUsuarioBD();
     await getCategoriasFilmes(db, API_KEY);
     await getFilmesPopulares(db, API_KEY);
 }
