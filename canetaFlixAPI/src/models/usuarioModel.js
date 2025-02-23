fb1d3dbad95ddf80caa9c6901888a5528169dd14
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-23 18:08:52 -0300

    Model de Usuários

---
+++ canetaFlixAPI/src/models/usuarioModel.js
@@ -0,0 +1,57 @@
+const db = require('../config/DBconnection.js');
+const bcrypt = require('bcrypt');
+
+const usuarioModel = {
+
+    cadastrarUser: async (nome, email, pw) => {
+        return new Promise((resolve, reject) => {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(pw, salt);
+            const data_cadastro = new Date().toISOString().split('T')[0];
+            const status = 'ativo';
+            const query = 'INSERT INTO usuarios (nome, email, pw, data_cadastro, status) VALUES (?, ?, ?, ?, ?)';
+            db.query(query, [nome, email, hash, data_cadastro, status], (err, results) => {
+                if (err) {
+                    console.error('Erro ao cadastrar usuário:', err);
+                    reject(err);
+                } else {
+                    console.log('Usuário cadastrado com sucesso:', results);
+                    resolve(results);
+                }
+            });
+        });
+    },
+    buscarUser: async (nome, pw) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM usuarios WHERE nome = ? AND pw = ?';
+            db.query(query, [nome, pw], (err, results) => {
+                if (err) {
+                    console.error('Erro ao buscar usuário:', err);
+                    reject(err);
+                } else {
+                    console.log('Usuário encontrado:', results);
+                    resolve(results);
+                }
+            });
+        });
+    },
+
+    removerUser: async (id_usuario, pw) => {
+
+        return new Promise((resolve, reject) => {
+            const query = 'DELETE FROM usuarios WHERE id_usuario = ? AND pw = ?';
+            db.query(query, [id_usuario, pw], (err, results) => {
+                if (err) {
+                    console.error('Erro ao remover usuário:', err);
+                    reject(err);
+                } else {
+                    console.log('Usuário removido com sucesso:', results);
+                    resolve(results);
+                }
+            });
+        });
+
+    }
+}
+
+module.exports = usuarioModel;
