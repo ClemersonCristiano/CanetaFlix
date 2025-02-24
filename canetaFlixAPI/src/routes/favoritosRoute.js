6601198e45c329011dbdacd873af053b3610e885
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-24 17:16:59 -0300

    mudança nas rotas

--- canetaFlixAPI/src/routes/favoritosRoute.js
+++ canetaFlixAPI/src/routes/favoritosRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const favoritosController = require('../controllers/favoritosController');
 
-router.get('/', favoritosController.listarFavoritos); // Rota GET para listar favoritos
+router.get('/:id_usuario', favoritosController.listarFavoritos); // Rota GET para listar favoritos
 
-router.get('/:id_usuario/:id_video_api', favoritosController.adicionarFavorito); // Rota para adicionar favorito
+router.get('/:id_usuario/adicionar/:id_video_api', favoritosController.adicionarFavorito); // Rota para adicionar favorito
 
-router.delete('/:id_usuario/:id_video_api', favoritosController.removerFavorito); // Rota para remover favorito
+router.delete('/:id_usuario/remover/:id_video_api', favoritosController.removerFavorito); // Rota para remover favorito
 
 module.exports = router;
