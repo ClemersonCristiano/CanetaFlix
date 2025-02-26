50a18d7fb4107028c16c0f2d076c9a644c9fb014
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-26 15:51:50 -0300

    ajustes no fetch

--- canetaFlixClient/assets/js/filme.js
+++ canetaFlixClient/assets/js/filme.js
@@ -1,31 +1,34 @@
 const id_video_api = new URLSearchParams(window.location.search).get('id');
-console.log(`ID do filme: ${id_video_api}`);
-
 const dadosFilme = document.getElementById('dadosFilme');
+const sessionToken = sessionStorage.getItem('token');
 
-fetch(`http://localhost:3000/api/videos/filmes`,{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('token')
-    },
-    body: JSON.stringify({ id_video_api })
-})
-.then(response => response.json())
-.then(json => {
-
-    const filme = json.dadosFilme;
-
-    dadosFilme.innerHTML = `
+if (!sessionStorage.getItem("token")) {
+    window.location.href = "login.html"; // Redireciona para o login
+} else{
+        fetch(`http://localhost:3000/api/videos/filmes`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: sessionToken
+            },
+        body: JSON.stringify({ id_video_api })
+        })
+        .then(response => response.json())
+        .then(json => {
+        
+        const filme = json.dadosFilme;
+        
+        dadosFilme.innerHTML = `
         
         <iframe src="${filme.url}" frameborder="0" allowfullscreen></iframe>
-
+        
         <section class="containerInfosFilme"> 
-            <h1>${filme.titulo}</h1>
-            <p>${filme.descricao}</p>
-            <p> Data de lançamento: ${filme.data_lancamento} </p>
-            <p> Categorias: ${filme.categorias} </p>
-        </section>
-    `;
-
-})
+        <h1>${filme.titulo}</h1>
+                <p>${filme.descricao}</p>
+                <p> Data de lançamento: ${filme.data_lancamento} </p>
+                <p> Categorias: ${filme.categorias} </p>
+                </section>
+                `;
+                
+            })
+}
--- canetaFlixClient/assets/js/home.js
+++ canetaFlixClient/assets/js/home.js
@@ -1,46 +1,51 @@
 const listagemFilmes = document.getElementById("listagemFilmes");
 const card = document.getElementsByClassName("cardFilme");
-
+const sessionToken = sessionStorage.getItem('token');
 async function listarFilmesHome(){
-    
-    fetch('http://localhost:3000/api/videos/filmes', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: localStorage.getItem('token')
-        }
-    })
-    .then(response => response.json())
-    .then(json => {
 
-        const filmes = json.dadosFilmes;
-
-        for (const filme of filmes) {
-            
-            listagemFilmes.innerHTML += `
-            
-                <picture class="cardFilme">
-            
+    if (!sessionStorage.getItem("token")) {
+        window.location.href = "login.html"; // Redireciona para o login
+    } else{
+           
+            fetch('http://localhost:3000/api/videos/filmes', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: sessionToken
+                }
+            })
+            .then(response => response.json())
+            .then(json => {
+                
+                const filmes = json.dadosFilmes;
+                
+                for (const filme of filmes) {
+                    
+                    listagemFilmes.innerHTML += `
+                
+                    <picture class="cardFilme">
+                    
                     <img src="${filme.thumbnail}">
                     <h2>${filme.titulo}</h2>
                     
-                </picture>
-            
-            `;
-
-        }
-
-        for (let i = 0; i < card.length; i++) {
-
-            card[i].addEventListener('click', () => {
-                window.location.href = `filme.html?id=${filmes[i].id}`;
-            });
-
-        }
-
-    })
-    .catch(error => console.error('Erro ao carregar a API:', error));
-  
+                    </picture>
+                    
+                    `;
+                    
+                }
+                
+                for (let i = 0; i < card.length; i++) {
+                    
+                    card[i].addEventListener('click', () => {
+                        window.location.href = `filme.html?id=${filmes[i].id}`;
+                    });
+                    
+                }
+                
+            })
+        .catch(error => console.error('Erro ao carregar a API:', error));
+        
+    }
 }
 
 listarFilmesHome();
