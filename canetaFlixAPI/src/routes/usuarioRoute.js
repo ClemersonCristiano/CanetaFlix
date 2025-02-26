e9ccce2c3303df4dff2c5bd83112a10c2274d370
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-26 02:42:21 -0300

    arquivo renomeado

---
+++ canetaFlixAPI/src/routes/usuarioRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const usuarioController = require('../controllers/usuarioController.js');
+const autenticarUsuario = require('../middleware/authMiddleware.js');
+
+router.get('/', usuarioController.Login); // Rota para login
+
+router.post('/', usuarioController.cadastrarUser); // Rota para Cadastrar usuário
+
+router.delete('/', autenticarUsuario, usuarioController.removerUser); // Rota para remover usuário
+
+module.exports = router;
