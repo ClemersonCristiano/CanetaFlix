d071aff97bb655836b85fae25b0e698836520a1a
Clemerson Cristiano <clemersoncristiano20@gmail.com>
2025-02-26 23:52:57 -0300

    ajuste de rota

--- canetaFlixAPI/src/routes/favoritosRoute.js
+++ canetaFlixAPI/src/routes/favoritosRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const favoritosController = require('../controllers/favoritosController');
 const autenticarUsuario = require('../middleware/authMiddleware.js');
 
-router.get('/', autenticarUsuario, favoritosController.listarFavoritos); // Rota GET para listar favoritos
+router.post('/listar', autenticarUsuario, favoritosController.listarFavoritos); // Rota para listar favoritos
 
 router.post('/', autenticarUsuario, favoritosController.adicionarFavorito); // Rota para adicionar favorito
 
--- canetaFlixAPI/src/routes/historicoRoute.js
+++ canetaFlixAPI/src/routes/historicoRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const historicoController = require('../controllers/historicoController');
 const autenticarUsuario = require('../middleware/authMiddleware.js');
 
-router.get('/', autenticarUsuario, historicoController.listarHistorico); // Rota GET para listar histórico
+router.post('/listar', autenticarUsuario, historicoController.listarHistorico); // Rota para listar histórico
 
 router.post('/', autenticarUsuario, historicoController.adicionarFilmeHistorico); // Rota para adicionar filme ao histórico
 
